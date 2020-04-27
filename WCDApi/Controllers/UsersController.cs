0eb3e93f13185c06b3dc4ae4ce7b12ecacb7b283
Marcin Marcinkowski <marcinek44@windowslive.com>
2020-04-27 21:42:10 +0200

    Added auth and registe method in controller

--- WCDApi/Controllers/UsersController.cs
+++ WCDApi/Controllers/UsersController.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using WCDApi.Entity;
 using WCDApi.Helpers;
+using WCDApi.Model.Users;
 using WCDApi.Services;
 
 namespace WCDApi.Controllers
@@ -28,5 +35,53 @@ namespace WCDApi.Controllers
             _mapper = mapper;
             _appSettings = appSettings.Value;
         }
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody]AuthenticateModel model)
+        {
+            var user = await _userService.Authenticate(model.Email, model.Password).ConfigureAwait(false);
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+            // return basic user info and authentication token
+            return Ok(new
+            {
+                Id = user.Id,
+                Role = user.Role,
+                Token = tokenString
+            });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody]RegisterModel model)
+        {
+            // map model to entity
+            var user = _mapper.Map<User>(model);
+            try
+            {
+                // create user
+                await _userService.Create(user).ConfigureAwait(false);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
     }
 }
