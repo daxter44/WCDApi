0f3ddbe35ef0e101b5137fdb84e24d3c9cf59733
Marcin Marcinkowski <marcinek44@windowslive.com>
2020-04-27 14:00:24 +0200

    Added DataContext

---
+++ WCDApi/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WCDApi.Entity;
+
+namespace WCDApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MonitoredItem> MonitoredItems { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MonitoredItem>().ToTable("MonitoredItem");
+            modelBuilder.Entity<User>().ToTable("Users");
+        }
+
+    }
+}
