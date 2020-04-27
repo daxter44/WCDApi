c3a6b30e058f938f459f92ec366b6ca30cb6d54d
Marcin Marcinkowski <marcinek44@windowslive.com>
2020-04-27 14:00:00 +0200

    Added entity User and Monitored Item

---
+++ WCDApi/Entity/MonitoredItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WCDApi.Entity
+{
+    public class MonitoredItem
+    {
+        public Guid MonitItemId { get; set; }
+        public string Url { get; set; }
+        public string ElementName { get; set; }
+        public int Frequency { get; set; }
+        public DateTime StartMonitDate { get; set; }
+        public DateTime EndMonitDate { get; set; }
+        public virtual User User { get; set; }
+    }
+}
