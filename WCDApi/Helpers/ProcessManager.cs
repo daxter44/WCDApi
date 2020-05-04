68fdc3dddf891c53503508540fb5e7c4d861d400
Daxter44 <marcinek44@windowslive.com>
2020-05-04 20:16:43 +0200

    Added processManager with Start and Stopprocess

---
+++ WCDApi/Helpers/ProcessManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WCDApi.Helpers
+{
+    public class ProcessManager
+    { 
+        public static int StartCommand()
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "./Worker/Worker", // or whatever shell you use
+                    UseShellExecute = false,
+                    CreateNoWindow = true   
+                }
+            };
+            process.OutputDataReceived += (sender, args) => Console.WriteLine("received output: {0}", args.Data);
+            process.Start();
+            return process.Id;
+        }
+
+        public static bool StopCommand(int processID)
+        {
+            var process = Process.GetProcessById(processID);
+            process.Kill();
+            return true;
+        }
+    }
+}
