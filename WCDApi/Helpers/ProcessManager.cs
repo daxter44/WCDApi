9001139834262a14efb3d40f35ee4c29e179d305
Daxter44 <marcinek44@windowslive.com>
2020-05-08 12:12:57 +0200

    Fix processStart in ProcessManager

--- WCDApi/Helpers/ProcessManager.cs
+++ WCDApi/Helpers/ProcessManager.cs
@@ -1,18 +1,22 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
+using WCDApi.DataBase.Entity;
 
 namespace WCDApi.Helpers
 {
     public class ProcessManager
     { 
-        public static int StartCommand()
+        public static int StartCommand(MonitoredItem monitoredItem)
         {
-
+            string args = PrepareArgs(monitoredItem);
             var process = new Process()
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = "./Worker/Worker", // or whatever shell you use
+                    FileName = "./Worker/WCDApi.Worker",
+                    Arguments = args,// or whatever shell you use
                     UseShellExecute = false,
                     CreateNoWindow = true   
                 }
@@ -27,6 +31,12 @@ namespace WCDApi.Helpers
             var process = Process.GetProcessById(processID);
             process.Kill();
             return true;
+        }
+
+        private static string PrepareArgs (MonitoredItem monitoredItem)
+        {
+            return "-u " + monitoredItem.Url + " -e " + monitoredItem.ElementName + " -f " + monitoredItem.Frequency.ToString() + " -i " + monitoredItem.MonitItemId.ToString();
+                    
         }
     }
 }
